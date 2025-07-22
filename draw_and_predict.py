8bb96cc87b31ad37aabc98f1d096846616bfe3c4
OmerYurttutan <109217812+omeryurttutan@users.noreply.github.com>
2025-07-22 14:52:24 +0300

    Update draw_and_predict.py

--- draw_and_predict.py
+++ draw_and_predict.py
@@ -4,10 +4,8 @@ from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 import tensorflow as tf
 
-# Modeli yükle
 model = tf.keras.models.load_model("mnist_model.h5")
 
-# Tkinter penceresi
 WIDTH, HEIGHT = 280, 280
 WHITE = (255, 255, 255)
 
@@ -42,16 +40,13 @@ class App:
         from PIL import ImageChops
         import matplotlib.pyplot as plt
 
-        # 1. Orijinal çizimi ters çevir
         img = self.image.copy()
         img = ImageOps.invert(img)
 
-        # 2. Rakamın etrafındaki boşluğu kırp
         bbox = img.getbbox()
         if bbox:
             img = img.crop(bbox)
 
-        # 3. Biraz boşluk bırak: padding (kenarlardan %20 kadar)
         pad_percent = 0.2
         w, h = img.size
         pad_w = int(w * pad_percent)
@@ -60,22 +55,18 @@ class App:
         padded_img = Image.new('L', (w + 2 * pad_w, h + 2 * pad_h), 0)
         padded_img.paste(img, (pad_w, pad_h))
 
-        # 4. Kare yap ve ortala
         max_side = max(padded_img.size)
         square_img = Image.new('L', (max_side, max_side), 0)
         offset = ((max_side - padded_img.size[0]) // 2, (max_side - padded_img.size[1]) // 2)
         square_img.paste(padded_img, offset)
 
-        # 5. 28x28'e küçült
         img_resized = square_img.resize((28, 28))
 
-        # 6. Göster (debug amaçlı)
         plt.imshow(img_resized, cmap='gray')
         plt.title("Ortalanmış + Kenarlı Görsel")
         plt.axis('off')
         plt.show()
 
-        # 7. Tahmin
         img_array = np.array(img_resized).astype('float32') / 255.0
         img_array = img_array.reshape(1, 28, 28)
         prediction = model.predict(img_array)
